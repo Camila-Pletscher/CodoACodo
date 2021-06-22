9b241dd8efed739b5fca0da122947bc50aa3e0b6
Camila-Pletscher <camipletscher@gmail.com>
2021-06-21 23:11:20 -0300

    Se hizo ejercicio unidad 2 js

---
+++ JavaScript/Unidad 2/codigo.js
@@ -0,0 +1,12 @@
+let alumno = prompt("Ingrese su nombre");
+
+let nota1 = prompt("Ingrese la primera nota");
+let nota2 = prompt("Ingrese la segunda nota");
+let nota3 = prompt("Ingrese la tercer nota");
+
+function promedio () {
+    promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
+    alert ("El promedio de notas es " + promedio);
+}
+
+promedio()
